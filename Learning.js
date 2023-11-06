28c22a95c6cac4d474aa661c1ea05ac9dee5c125
ehdrma <ehdrma99@gmail.com>
2023-11-07 04:15:27 +0900

    bb

--- Learning.js
+++ Learning.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
-import { DrawerActions } from '@react-navigation/native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, Alert, ActivityIndicator } from 'react-native';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+import { Audio } from 'expo-av';
 
 const Learning = ({ route }) => {
     const { article } = route.params;
@@ -9,6 +9,9 @@ const Learning = ({ route }) => {
     const [isTranslated, setIsTranslated] = useState(true);
     const [showOriginal, setShowOriginal] = useState(true);
     const [bodyContent, setBodyContent] = useState(article.body);
+    const [isLoading, setIsLoading] = useState(false);
+    const [sound, setSound] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     useEffect(() => {
         setBodyContent(showOriginal ? article.body : isTranslated ? article.translatedText : article.body);
@@ -19,7 +22,54 @@ const Learning = ({ route }) => {
     };
 
     const handleBackPress = () => {
+        if (sound) {
+            sound.unloadAsync(); // 화면을 벗어날 때 음원을 해제합니다.
+        }
         navigation.goBack();
+    };
+
+    const handleTranslateToggle = async () => {
+        setShowOriginal(!showOriginal);
+    };
+
+    const handlePlayAudio = async () => {
+        setIsLoading(true);
+        try {
+            if (sound) {
+                if (isPlaying) {
+                    await sound.pauseAsync();
+                } else {
+                    await sound.playAsync();
+                }
+                setIsPlaying(!isPlaying);
+            } else {
+                const response = await fetch('http://192.168.219.107:5000/text-to-speech', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        text: article.body,
+                    }),
+                });
+
+                if (response.ok) {
+                    const { audioUrl } = await response.json();
+                    const { sound } = await Audio.Sound.createAsync({ uri: audioUrl });
+                    setSound(sound);
+                    await sound.playAsync();
+                    setIsPlaying(true);
+                } else {
+                    const errorResponse = await response.json();
+                    Alert.alert('음성 변환 오류', errorResponse.error);
+                }
+            }
+        } catch (error) {
+            console.error('음성 변환 오류:', error);
+            Alert.alert('음성 변환 오류', '음성 변환 중 오류가 발생했습니다.');
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     return (
@@ -32,7 +82,7 @@ const Learning = ({ route }) => {
                     <Image source={require('./assets/back.png')} style={styles.back} resizeMode="contain" />
                 </TouchableOpacity>
             </View>
-            <ScrollView contentContainerStyle={styles.scrollViewContent}>
+            <ScrollView contentContainerStyle={styles.scrollViewContent} showsVerticalScrollIndicator={false}>
                 <Text style={styles.title}>
                     {showOriginal
                         ? article.title
@@ -45,16 +95,54 @@ const Learning = ({ route }) => {
                         {bodyContent}
                     </Text>
                 </View>
+                <View style={styles.buttonContainer}>
                 <TouchableOpacity
-                    style={styles.translateButton}
-                    onPress={() => setShowOriginal(!showOriginal)}
-                >
-                    <View style={[styles.translateButtonInner, { backgroundColor: showOriginal ? '#990011' : '#d9d9d9' }]}>
-                        <Text style={[styles.translateButtonText, { color: showOriginal ? '#ffffff' : '#282828' }]}>
-                            {showOriginal ? '번역보기' : isTranslated ? '원문보기' : '번역보기'}
+                        style={[
+                            styles.button,
+                            {
+                                backgroundColor: isLoading ? '#d9d9d9' : isPlaying ? '#990011' : '#d9d9d9',
+                            },
+                        ]}
+                        onPress={handlePlayAudio}
+                        disabled={isLoading}
+                    >
+                        {isLoading ? (
+                            <ActivityIndicator size="small" color="#990011" />
+                        ) : (
+                            <Text
+                                style={[
+                                    styles.buttonText,
+                                    {
+                                        color: isLoading ? '#ffffff' : isPlaying ? '#ffffff' : '#282828',
+                                    },
+                                ]}
+                            >
+                                {isLoading ? '음성 변환 중...' : isPlaying ? '일시 정지' : '원어 듣기'}
+                            </Text>
+                        )}
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.button,
+                            {
+                                backgroundColor: showOriginal ? '#d9d9d9' : isTranslated ? '#990011' : '#d9d9d9',
+                            },
+                        ]}
+                        onPress={handleTranslateToggle}
+                    >
+                        <Text
+                            style={[
+                                styles.buttonText,
+                                {
+                                    color: showOriginal ? '#282828' : isTranslated ? '#ffffff' : '#282828',
+                                },
+                            ]}
+                        >
+                            {showOriginal ? '번역 보기' : isTranslated ? '원문 보기' : '번역 보기'}
                         </Text>
-                    </View>
-                </TouchableOpacity>
+                    </TouchableOpacity>
+                    
+                </View>
             </ScrollView>
         </View>
     );
@@ -110,16 +198,17 @@ const styles = StyleSheet.create({
         fontSize: 16,
         letterSpacing: 1.5,
     },
-    translateButton: {
-        marginTop: 15,
-        alignSelf: 'flex-end',
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
     },
-    translateButtonInner: {
+    button: {
         borderRadius: 25,
         paddingVertical: 11,
         paddingHorizontal: 16,
     },
-    translateButtonText: {
+    buttonText: {
         fontSize: 15,
         fontWeight: 'bold',
     },
